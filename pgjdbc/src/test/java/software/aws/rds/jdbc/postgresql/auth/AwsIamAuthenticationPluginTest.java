ee883d1eb012b97c267d49df6a12a831d8b506f5
alecc <alecc@bitquilltech.com>
2022-06-20 12:20:20 -0700

    moved duplicate code into BeforeEach method, changed hard coded strings into global variables

--- pgjdbc/src/test/java/software/aws/rds/jdbc/postgresql/auth/AwsIamAuthenticationPluginTest.java
+++ pgjdbc/src/test/java/software/aws/rds/jdbc/postgresql/auth/AwsIamAuthenticationPluginTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeEach;
+
 import org.postgresql.PGProperty;
 import org.postgresql.plugin.AuthenticationRequestType;
 import org.postgresql.util.PSQLException;
@@ -21,71 +23,71 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class AwsIamAuthenticationPluginTest {
 
+  private Properties properties;
+  private AwsIamAuthenticationPlugin plugin;
+  private static final String TEST_USER = "testUser";
+  private static final String TEST_HOST = "test.user.us-east-2.rds.amazonaws.com";
+  private static final String TEST_TOKEN = "testToken";
+  private static final String TOKEN_FIELD = "tokenCache";
+  private static final String GENERATED_TOKEN = "generatedToken";
+  private static final String CACHE_KEY = "us-east-2:test.user.us-east-2.rds.amazonaws.com:5342:testUser";
+
+  @BeforeEach
+  public void init() throws PSQLException {
+    properties = new Properties();
+    properties.setProperty(PGProperty.USER.getName(), TEST_USER);
+    properties.setProperty(PGProperty.PG_HOST.getName(), TEST_HOST);
+    plugin = new AwsIamAuthenticationPlugin(properties);
+  }
+
   @Test
   public void testGetPasswordWithWrongType() throws PSQLException {
-    Properties properties = new Properties();
-    properties.setProperty(PGProperty.USER.getName(), "testUser");
-    properties.setProperty(PGProperty.PG_HOST.getName(), "test.user.us-east-2.rds.amazonaws.com");
-    AwsIamAuthenticationPlugin plugin = new AwsIamAuthenticationPlugin(properties);
     assertThrows(PSQLException.class, () -> plugin.getPassword(AuthenticationRequestType.MD5_PASSWORD));
   }
 
   @Test
   public void testGetPasswordValidTokenInCache() throws PSQLException, NoSuchFieldException,
       IllegalAccessException {
-    Properties properties = new Properties();
-    properties.setProperty(PGProperty.USER.getName(), "testUser");
-    properties.setProperty(PGProperty.PG_HOST.getName(), "test.user.us-east-2.rds.amazonaws.com");
-    AwsIamAuthenticationPlugin plugin = new AwsIamAuthenticationPlugin(properties);
-    plugin.getClass().getDeclaredField("tokenCache").setAccessible(true);
-    Field tokenField = plugin.getClass().getDeclaredField("tokenCache");
+    plugin.getClass().getDeclaredField(TOKEN_FIELD).setAccessible(true);
+    Field tokenField = plugin.getClass().getDeclaredField(TOKEN_FIELD);
     tokenField.setAccessible(true);
     Field modifiersField = Field.class.getDeclaredField("modifiers");
     modifiersField.setAccessible(true);
     modifiersField.setInt(tokenField, tokenField.getModifiers() & ~Modifier.FINAL);
     ConcurrentHashMap<String, AwsIamAuthenticationPlugin.TokenInfo> value = new ConcurrentHashMap<>();
     value.clear();
-    value.put("us-east-2:test.user.us-east-2.rds.amazonaws.com:5342:testUser", new AwsIamAuthenticationPlugin.TokenInfo("testToken", Instant.now().plusMillis(300000)));
+    value.put(CACHE_KEY, new AwsIamAuthenticationPlugin.TokenInfo(TEST_TOKEN, Instant.now().plusMillis(300000)));
     tokenField.set(plugin, value);
 
     char[] actualResult = plugin.getPassword(AuthenticationRequestType.CLEARTEXT_PASSWORD);
-    assertArrayEquals("testToken".toCharArray(), actualResult);
+    assertArrayEquals(TEST_TOKEN.toCharArray(), actualResult);
   }
 
   @Test
   public void testGetPasswordExpiredTokenInCache() throws PSQLException, NoSuchFieldException,
       IllegalAccessException {
-    Properties properties = new Properties();
-    properties.setProperty(PGProperty.USER.getName(), "testUser");
-    properties.setProperty(PGProperty.PG_HOST.getName(), "test.user.us-east-2.rds.amazonaws.com");
-    AwsIamAuthenticationPlugin plugin = new AwsIamAuthenticationPlugin(properties);
-    plugin.getClass().getDeclaredField("tokenCache").setAccessible(true);
-    Field tokenField = plugin.getClass().getDeclaredField("tokenCache");
+    plugin.getClass().getDeclaredField(TOKEN_FIELD).setAccessible(true);
+    Field tokenField = plugin.getClass().getDeclaredField(TOKEN_FIELD);
     tokenField.setAccessible(true);
     Field modifiersField = Field.class.getDeclaredField("modifiers");
     modifiersField.setAccessible(true);
     modifiersField.setInt(tokenField, tokenField.getModifiers() & ~Modifier.FINAL);
     ConcurrentHashMap<String, AwsIamAuthenticationPlugin.TokenInfo> value = new ConcurrentHashMap<>();
     value.clear();
-    value.put("us-east-2:test.user.us-east-2.rds.amazonaws.com:5342:testUser", new AwsIamAuthenticationPlugin.TokenInfo("testToken", Instant.now().minusMillis(300000)));
+    value.put(CACHE_KEY, new AwsIamAuthenticationPlugin.TokenInfo(TEST_TOKEN, Instant.now().minusMillis(300000)));
     tokenField.set(plugin, value);
 
     AwsIamAuthenticationPlugin spyPlugin = Mockito.spy(plugin);
-    when(spyPlugin.generateAuthenticationToken("testUser", "test.user.us-east-2.rds.amazonaws.com", 5342, Region.US_EAST_2)).thenReturn("generatedToken");
+    when(spyPlugin.generateAuthenticationToken(TEST_USER, TEST_HOST, 5342, Region.US_EAST_2)).thenReturn(GENERATED_TOKEN);
     char[] actualResult = spyPlugin.getPassword(AuthenticationRequestType.CLEARTEXT_PASSWORD);
-    assertArrayEquals("generatedToken".toCharArray(), actualResult);
+    assertArrayEquals(GENERATED_TOKEN.toCharArray(), actualResult);
   }
 
   @Test
   public void testGetPasswordGenerateToken() throws PSQLException, NoSuchFieldException,
       IllegalAccessException {
-    Properties properties = new Properties();
-    properties.setProperty(PGProperty.USER.getName(), "testUser");
-    properties.setProperty(PGProperty.PG_HOST.getName(), "test.user.us-east-2.rds.amazonaws.com");
-
-    AwsIamAuthenticationPlugin plugin = new AwsIamAuthenticationPlugin(properties);
-    plugin.getClass().getDeclaredField("tokenCache").setAccessible(true);
-    Field tokenField = plugin.getClass().getDeclaredField("tokenCache");
+    plugin.getClass().getDeclaredField(TOKEN_FIELD).setAccessible(true);
+    Field tokenField = plugin.getClass().getDeclaredField(TOKEN_FIELD);
     tokenField.setAccessible(true);
     Field modifiersField = Field.class.getDeclaredField("modifiers");
     modifiersField.setAccessible(true);
@@ -95,9 +97,9 @@ public class AwsIamAuthenticationPluginTest {
     tokenField.set(plugin, value);
 
     AwsIamAuthenticationPlugin spyPlugin = Mockito.spy(new AwsIamAuthenticationPlugin(properties));
-    when(spyPlugin.generateAuthenticationToken("testUser", "test.user.us-east-2.rds.amazonaws.com", 5342, Region.US_EAST_2)).thenReturn("generatedToken");
+    when(spyPlugin.generateAuthenticationToken(TEST_USER, TEST_HOST, 5342, Region.US_EAST_2)).thenReturn(GENERATED_TOKEN);
     char[] actualResult = spyPlugin.getPassword(AuthenticationRequestType.CLEARTEXT_PASSWORD);
-    assertArrayEquals("generatedToken".toCharArray(), actualResult);
+    assertArrayEquals(GENERATED_TOKEN.toCharArray(), actualResult);
   }
 
 }
